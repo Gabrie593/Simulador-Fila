b112414de4167d3217c1a04b424db33717de3778
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2019-07-08 13:31:36 -0300

    fila final

--- fila.py
+++ fila.py
@@ -1,68 +1,46 @@
-line = []
-linepregnant = []
-lineold = []
-people = 0
-aux = 0
-longest = 1
-environment = '''
-______________________
-|     PythonBank     |
-|____________________|
+enviroment = '''
+_____________
+Python Bank |
+      $     |
+------------|
+            |
+       o]   | {0} --- Normal People ---
+            |
+       o]   | {1} --- Pregnant People ---
+            |
+       o]   | {2} --- Old People ---
+____________|
 '''
-message = int(input("To add someone in the line, press 1, to make the line walk, press 0: "))
-if message == 1:
-    preference = int(input("If you have more than 59 years, press 2, if you are pregnant, press 3, else, press 4: "))
-    while True:
-        if preference == 4: # Colocando não preferenciais na fila
-                people += 1
-                line.append(people)
-                x = len(line)
-                y = len(lineold)
-                z = len(linepregnant)
-                longest = max(x, y, z)
-                while aux < longest:
-                    print(environment)
-                    if len(line) >= aux:
-                        print("\t", (line[aux]))
-                    if len(linepregnant) >= aux:
-                        print("\t \t", (linepregnant[aux]))
-                    if len(lineold) >= aux:
-                        print("\t \t \t", (lineold[aux]))
-                    aux += 1
-        if preference == 3: # Colocando grávidas na fila
-                people += 1
-                linepregnant.append(people)
-                x = len(line)
-                y = len(lineold)
-                z = len(linepregnant)
-                while aux < longest:
-                    print(environment)
-                    if len(line) >= aux:
-                        print("\t", (line[aux]))
-                    if len(linepregnant) >= aux:
-                        print("\t \t", (linepregnant[aux]))
-                    if len(lineold) >= aux:
-                        print("\t \t \t", (lineold[aux]))
-                    aux += 1
-        if preference == 2: # Colocando idosos na fila
-                people += 1
-                lineold.append(people)
-                x = len(line)
-                y = len(lineold)
-                z = len(linepregnant)
-                while aux < longest:
-                    print(environment)
-                    if len(line) >= aux:
-                        print("\t", (line[aux]))
-                    if len(linepregnant) >= aux:
-                        print("\t \t", (linepregnant[aux]))
-                    if len(lineold) >= aux:
-                        print("\t \t \t", (lineold[aux]))
-                    aux += 1
-        message = int(input("To add someone in the line, press 1, to make the line walk, press 0: "))
-        aux = 0
-        if message == 1:
-            preference = int(input("If you have more than 59 years, press 2, if you are pregnant, press 3, else, press 4: "))
+line = []
+lineold = []
+linepregnant = []
+people = 1
+
+while True:
+    welcome = (input("Welcome to python bank! To add someone to the line press 'A', to make the line walk, press 'W': ")).upper()
+    if welcome == "W":
+        if len(lineold) > 0:
+            print("O cliente número {0} foi atendito".format(lineold[0]))
+            lineold.pop(0)
+        elif len(linepregnant) > 0:
+            print("O cliente número {0} foi atendito".format(linepregnant[0]))
+            linepregnant.pop(0)
+        elif len(line) > 0:
+            print("O cliente número {0} foi atendito".format(line[0]))
+            line.pop(0)
+    if welcome == "A":
+        preference = int(input("If you have more than 59 years, press '0', if you are pregnant, press '1', else, press '2': "))
+        if preference == 0:
+            lineold.append(people)
+            people += 1
+        if preference == 1:
+            linepregnant.append(people)
+            people += 1
+        if preference == 2:
+            line.append(people)
+            people += 1
+
+    print(enviroment.format(line, linepregnant, lineold))
 
 
 
